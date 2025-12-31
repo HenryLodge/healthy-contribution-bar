4bc97593c8b360a50b871c41f30e0bf1cbe7d04d
HenryLodge <hlcodes01@gmail.com>
2025-12-31 04:57:38 -0500

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,72 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { resolve } from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       input: {
+//         popup: resolve(__dirname, 'index.html'),
+//         content: resolve(__dirname, 'public/content.js'),
+//         'service-worker': resolve(__dirname, 'public/service-worker.js'),
+//       },
+//       output: {
+//         entryFileNames: (chunkInfo) => {
+//           if (chunkInfo.name === 'popup') {
+//             return 'assets/[name]-[hash].js'
+//           }
+//           return '[name].js'
+//         },
+//       }
+//     }
+//   }
+// })
+
+
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+        content: resolve(__dirname, 'public/content.js'),
+        'service-worker': resolve(__dirname, 'public/service-worker.js'),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'popup') {
+            return 'assets/[name]-[hash].js'
+          }
+          return '[name].js'
+        },
+        inlineDynamicImports: false
+      }
+    }
+  }
 })
